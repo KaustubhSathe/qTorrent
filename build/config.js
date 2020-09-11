4649de5d533cf424557242da2fe4424a7119aa0d
Kaustubh Sathe <f2015466@pilani.bits-pilani.ac.in>
2020-09-11 14:45:30 +0530

    Delete config.js

--- build/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectionURL = void 0;
-exports.connectionURL = "mongodb+srv://kaustubh:Sekizo7777@qtorrent-pvg1r.mongodb.net/store?retryWrites=true&w=majority";
-//# sourceMappingURL=config.js.map
